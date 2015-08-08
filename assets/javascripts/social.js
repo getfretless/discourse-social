084c6d59696c5d3daa81f7ef1abdb7ec95983741
David Jones <unixmonkey1@gmail.com>
2015-08-08 15:36:37 -0400

    Make a single button with all share links

--- assets/javascripts/social.js
+++ assets/javascripts/social.js
@@ -22,6 +22,22 @@
     }
   });
 
+  Discourse.FacebookLink = Discourse.View.extend(Discourse.StringBuffer, {
+    tagName: 'a',
+    classNames: ['share-facebook'],
+    attributeBindings: ['title'],
+    title: I18n.t('share.facebook'),
+    click: function() {
+      var title = this.get('controller.content.title');
+      var link = Discourse.AbsolutePath(this.get('controller.content.url'));
+      var url = "http://www.facebook.com/sharer.php?u=" + encodeURIComponent(link) + '&t=' + encodeURIComponent(title);
+      Discourse.OpenSharingPopup(url);
+    },
+    renderString: function(buffer) {
+      buffer.push("<i class='fa fa-facebook-square'></i>");
+    }
+  });
+
   Discourse.TwitterButton = Discourse.ButtonView.extend({
     text: 'Tweet',
     title: I18n.t('share.twitter'),
@@ -32,6 +48,22 @@
       Discourse.OpenSharingPopup(url, 265);
     },
     renderIcon: function(buffer) {
+      buffer.push("<i class='fa fa-twitter-square'></i>");
+    }
+  });
+
+  Discourse.TwitterLink = Ember.View.extend(Discourse.StringBuffer, {
+    tagName: 'a',
+    classNames: ['share-twitter'],
+    attributeBindings: ['title'],
+    title: I18n.t('share.twitter'),
+    click: function() {
+      var title = this.get('controller.content.title');
+      var link = Discourse.AbsolutePath(this.get('controller.content.url'));
+      var url = "http://twitter.com/intent/tweet?url=" + encodeURIComponent(link) + "&text=" + encodeURIComponent(title);
+      Discourse.OpenSharingPopup(url, 265);
+    },
+    renderString: function(buffer) {
       buffer.push("<i class='fa fa-twitter-square'></i>");
     }
   });
@@ -49,8 +81,24 @@
     }
   });
 
+  Discourse.GoogleLink = Ember.View.extend(Discourse.StringBuffer, {
+    tagName: 'a',
+    classNames: ['share-google'],
+    attributeBindings: ['title'],
+    title: I18n.t('share.google+'),
+    click: function() {
+      var link = Discourse.AbsolutePath(this.get('controller.content.url'));
+      var url = "https://plus.google.com/share?url=" + encodeURIComponent(link);
+      Discourse.OpenSharingPopup(url, 600);
+    },
+    renderString: function(buffer) {
+      buffer.push("<i class='fa fa-google-plus-square'></i>");
+    }
+  });
+
   Discourse.EmailButton = Discourse.ButtonView.extend({
     text: 'Email',
+    classNames: ['share-email'],
     title: I18n.t('share.email'),
     click: function() {
       var title = this.get('controller.content.title');
@@ -64,12 +112,43 @@
     }
   });
 
+  Discourse.EmailLink = Ember.View.extend(Discourse.StringBuffer, {
+    tagName: 'a',
+    classNames: ['share-email'],
+    attributeBindings: ['title'],
+    title: I18n.t('share.email'),
+    click: function() {
+      var title = this.get('controller.content.title');
+      var subject = encodeURIComponent('[' + Discourse.SiteSettings.title + '] ' + title);
+      var link = Discourse.AbsolutePath(this.get('controller.content.url'));
+      var url = "mailto:?to=&subject=" + subject + "&body=" + encodeURIComponent(link);
+      window.open(url, '_blank');
+    },
+    renderString: function(buffer) {
+      buffer.push("<i class='fa fa-envelope-square'></i>");
+    }
+  });
+
+  Discourse.MegaSocialButtonView = Ember.View.create({
+    tagName: 'button',
+    classNames: ['btn', 'btn-standard'],
+    templateName: 'social-button',
+    elementId: 'mega-social-button',
+    name: 'megaSocial',
+    sources: [Discourse.TwitterLink, Discourse.FacebookLink, Discourse.GoogleLink, Discourse.EmailLink]
+  });
+
   Discourse.TopicFooterButtonsView.reopen({
     addAdditionalButtons: function() {
       this.attachViewClass(Discourse.FacebookButton);
       this.attachViewClass(Discourse.TwitterButton);
       this.attachViewClass(Discourse.GoogleButton);
       this.attachViewClass(Discourse.EmailButton);
+      this.attachViewClass(Discourse.MegaSocialButtonView);
+      setTimeout(function() {
+        // replace existing share button
+        $('#topic-footer-buttons .btn.share').replaceWith($('#mega-social-button'));
+      }, 50);
     }.on("additionalButtons")
   });
 
--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,7 @@
 # name: social
 # about: add social buttons at the end of the topics
-# version: 0.1
+# version: 0.2
 # authors: David Jones
 
 register_asset 'javascripts/social.js'
+register_asset 'javascripts/social-button.hbs'
